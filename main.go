f95aa5f0e39b85198655e8399c161fc5e6cd8a59
Sebastien Douheret <sebastien.douheret@iot.bzh>
2017-11-30 16:09:01 +0100

    Added Copyright headers.
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
-// xds-cli: command line tool used to control / interface X(cross) Development System.
+/*
+ * Copyright (C) 2017 "IoT.bzh"
+ * Author Sebastien Douheret <sebastien@iot.bzh>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * xds-cli: command line tool used to control / interface X(cross) Development System.
+ */
+
 package main
 
 import (
@@ -37,7 +56,7 @@ var AppSubVersion = "unknown-dev"
 
 // Application details
 const (
-	appCopyright    = "Apache-2.0"
+	appCopyright    = "Copyright (C) 2017 IoT.bzh - Apache-2.0"
 	defaultLogLevel = "error"
 )
 
